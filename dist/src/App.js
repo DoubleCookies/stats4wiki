8b5c94971ac0f073aa9c13d55439d0be1a8892b5
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2021-05-17 17:08:56 +0300

    Created page, add new ico, add some information, packages update

--- dist/src/App.js
+++ dist/src/App.js
@@ -1,16 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <p>
-              Work in progress ~(˘▾˘~)
-          </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <header className="app-header">
+                Stats4wiki<br/>~(˘▾˘~)
+            </header>
+            <main className="main-content">
+                <div className="main-headers">О чём этот проект?</div>
+                <br/>
+                *Coming soon*
+                <div className="main-headers">Как он работает?</div>
+                <br/>
+                *Coming soon*
+            </main>
+            <footer className="footer-block">
+                <div>2018-2021 <span role="img" aria-label="cookieEmoji">🍪</span></div>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
